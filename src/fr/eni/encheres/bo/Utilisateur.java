0a681013f616cf0fb227eb5640de9819d1624d15
Dorig <dorig.bernard.db@gmail.com>
2020-04-07 15:03:43 +0200

    Création classes - Implémentation de la methode find

--- src/fr/eni/encheres/bo/Utilisateur.java
+++ src/fr/eni/encheres/bo/Utilisateur.java
@@ -23,4 +23,201 @@ public class Utilisateur {
     private boolean administrateur;
     private List<Enchere> encheres;
     private List<Article> articles;
+	/**
+	 * Getter pour noUtilisateur.
+	 * @return the noUtilisateur
+	 */
+	public int getNoUtilisateur() {
+		return noUtilisateur;
+	}
+	/**
+	 * Setter pour noUtilisateur.
+	 * @param noUtilisateur the noUtilisateur to set
+	 */
+	public void setNoUtilisateur(int noUtilisateur) {
+		this.noUtilisateur = noUtilisateur;
+	}
+	/**
+	 * Getter pour pseudo.
+	 * @return the pseudo
+	 */
+	public String getPseudo() {
+		return pseudo;
+	}
+	/**
+	 * Setter pour pseudo.
+	 * @param pseudo the pseudo to set
+	 */
+	public void setPseudo(String pseudo) {
+		this.pseudo = pseudo;
+	}
+	/**
+	 * Getter pour nom.
+	 * @return the nom
+	 */
+	public String getNom() {
+		return nom;
+	}
+	/**
+	 * Setter pour nom.
+	 * @param nom the nom to set
+	 */
+	public void setNom(String nom) {
+		this.nom = nom;
+	}
+	/**
+	 * Getter pour prenom.
+	 * @return the prenom
+	 */
+	public String getPrenom() {
+		return prenom;
+	}
+	/**
+	 * Setter pour prenom.
+	 * @param prenom the prenom to set
+	 */
+	public void setPrenom(String prenom) {
+		this.prenom = prenom;
+	}
+	/**
+	 * Getter pour email.
+	 * @return the email
+	 */
+	public String getEmail() {
+		return email;
+	}
+	/**
+	 * Setter pour email.
+	 * @param email the email to set
+	 */
+	public void setEmail(String email) {
+		this.email = email;
+	}
+	/**
+	 * Getter pour telephone.
+	 * @return the telephone
+	 */
+	public String getTelephone() {
+		return telephone;
+	}
+	/**
+	 * Setter pour telephone.
+	 * @param telephone the telephone to set
+	 */
+	public void setTelephone(String telephone) {
+		this.telephone = telephone;
+	}
+	/**
+	 * Getter pour rue.
+	 * @return the rue
+	 */
+	public String getRue() {
+		return rue;
+	}
+	/**
+	 * Setter pour rue.
+	 * @param rue the rue to set
+	 */
+	public void setRue(String rue) {
+		this.rue = rue;
+	}
+	/**
+	 * Getter pour codePostal.
+	 * @return the codePostal
+	 */
+	public String getCodePostal() {
+		return codePostal;
+	}
+	/**
+	 * Setter pour codePostal.
+	 * @param codePostal the codePostal to set
+	 */
+	public void setCodePostal(String codePostal) {
+		this.codePostal = codePostal;
+	}
+	/**
+	 * Getter pour ville.
+	 * @return the ville
+	 */
+	public String getVille() {
+		return ville;
+	}
+	/**
+	 * Setter pour ville.
+	 * @param ville the ville to set
+	 */
+	public void setVille(String ville) {
+		this.ville = ville;
+	}
+	/**
+	 * Getter pour motDePasse.
+	 * @return the motDePasse
+	 */
+	public String getMotDePasse() {
+		return motDePasse;
+	}
+	/**
+	 * Setter pour motDePasse.
+	 * @param motDePasse the motDePasse to set
+	 */
+	public void setMotDePasse(String motDePasse) {
+		this.motDePasse = motDePasse;
+	}
+	/**
+	 * Getter pour credit.
+	 * @return the credit
+	 */
+	public int getCredit() {
+		return credit;
+	}
+	/**
+	 * Setter pour credit.
+	 * @param credit the credit to set
+	 */
+	public void setCredit(int credit) {
+		this.credit = credit;
+	}
+	/**
+	 * Getter pour administrateur.
+	 * @return the administrateur
+	 */
+	public boolean isAdministrateur() {
+		return administrateur;
+	}
+	/**
+	 * Setter pour administrateur.
+	 * @param administrateur the administrateur to set
+	 */
+	public void setAdministrateur(boolean administrateur) {
+		this.administrateur = administrateur;
+	}
+	/**
+	 * Getter pour encheres.
+	 * @return the encheres
+	 */
+	public List<Enchere> getEncheres() {
+		return encheres;
+	}
+	/**
+	 * Setter pour encheres.
+	 * @param encheres the encheres to set
+	 */
+	public void setEncheres(List<Enchere> encheres) {
+		this.encheres = encheres;
+	}
+	/**
+	 * Getter pour articles.
+	 * @return the articles
+	 */
+	public List<Article> getArticles() {
+		return articles;
+	}
+	/**
+	 * Setter pour articles.
+	 * @param articles the articles to set
+	 */
+	public void setArticles(List<Article> articles) {
+		this.articles = articles;
+	}
+    
 }
--- src/fr/eni/encheres/dal/UtilisateurDAOImpl.java
+++ src/fr/eni/encheres/dal/UtilisateurDAOImpl.java
@@ -3,7 +3,12 @@
  */
 package fr.eni.encheres.dal;
 
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
 import java.util.ArrayList;
+
+import fr.eni.encheres.bo.Utilisateur;
 
 /**
  * Classe en charge de
@@ -12,6 +17,8 @@ import java.util.ArrayList;
  * @date 7 avr. 2020
  */
 public class UtilisateurDAOImpl implements DAO{
+	
+	private static ConnectionProvider connectionProvider = new ConnectionProvider();
 
 	/**
 	 * {@inheritDoc}
@@ -49,8 +56,33 @@ public class UtilisateurDAOImpl implements DAO{
 	 */
 	@Override
 	public Object find(int id) {
-		// TODO Auto-generated method stub
-		return null;
+		String REQUETE = "SELECT * " + 
+				"FROM ENCHERES " +
+				"WHERE no_utilisateur=?";
+		Utilisateur utilisateur = new Utilisateur();
+		try(Connection cnx = connectionProvider.getInstance().preparedStarement){
+			PreparedStatement pstmt = cnx.prepareStatement(REQUETE);
+			pstmt.setInt(1, id);
+			ResultSet rs = pstmt.executeQuery();
+			while(rs.next()) {
+				utilisateur.setNoUtilisateur(rs.getInt("no_utilisateur"));
+				utilisateur.setPseudo(rs.getString("pseudo"));
+				utilisateur.setNom(rs.getString("nom"));
+				utilisateur.setPrenom(rs.getString("prenom"));
+				utilisateur.setEmail(rs.getString("email"));
+				utilisateur.setTelephone(rs.getString("telephone"));
+				utilisateur.setRue(rs.getString("rue"));
+				utilisateur.setCodePostal(rs.getString("code_postal"));
+				utilisateur.setVille(rs.getString("ville"));
+				utilisateur.setMotDePasse(rs.getString("mot_de_passe"));
+				utilisateur.setCredit(rs.getInt("credit"));
+				utilisateur.setAdministrateur(rs.getBoolean("administrateur"));
+			}
+		}
+		catch(Exception e) {
+			e.printStackTrace();
+		}
+		return utilisateur;
 	}
 
 	/**
