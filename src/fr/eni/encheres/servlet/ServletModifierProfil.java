33e0468f4d5618cfee2c623ad9b3c70ff0810d6f
Dorig <dorig.bernard.db@gmail.com>
2020-04-08 18:55:00 +0200

    Modifier Profil - Déplacement dans doPost

--- src/fr/eni/encheres/servlet/ServletModifierProfil.java
+++ src/fr/eni/encheres/servlet/ServletModifierProfil.java
@@ -30,48 +30,44 @@ public class ServletModifierProfil extends HttpServlet {
 	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
 	 */
 	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-		if(request.getParameter("id")==null) {
-			utilisateur = utilisateurDAOImpl.find(Integer.valueOf(request.getParameter("utilisateur")));
-			utilisateur.setPseudo(request.getParameter("pseudo"));
-			utilisateur.setNom(request.getParameter("nom"));
-			utilisateur.setPrenom(request.getParameter("prenom"));
-			utilisateur.setEmail(request.getParameter("email"));
-			utilisateur.setTelephone(request.getParameter("telephone"));
-			utilisateur.setRue(request.getParameter("rue"));
-			utilisateur.setCodePostal(request.getParameter("codePostal"));
-			utilisateur.setVille(request.getParameter("ville"));
-			if(!request.getParameter("newmdp").equals("")) {
-				if(request.getParameter("mdp").equals(utilisateur.getMotDePasse())) {
-					if(request.getParameter("newmdp").equals(request.getParameter("confirmation"))) {
-						utilisateur.setMotDePasse(request.getParameter("newmdp"));
-					}else {
-						request.setAttribute("message", "Votre mot de passe de confirmation et votre nouveau mot de passe ne correspondent pas.");
-						request.setAttribute("utilisateur", utilisateur);
-						this.getServletContext().getRequestDispatcher("/WEB-INF/modifierProfil.jsp").forward(request, response);
-					}
-				}else {
-					request.setAttribute("message", "Votre mot de passe actuel est incorrect.");
-					request.setAttribute("utilisateur", utilisateur);
-					this.getServletContext().getRequestDispatcher("/WEB-INF/modifierProfil.jsp").forward(request, response);
-				}
-			}
-			UtilisateurDAOImpl utilisateurDAOImpl = new UtilisateurDAOImpl();
-			utilisateurDAOImpl.update(utilisateur);
-			request.setAttribute("message", "Votre profil a été modifié avec succès.");
-			request.setAttribute("utilisateur", utilisateur);
-			this.getServletContext().getRequestDispatcher("/WEB-INF/modifierProfil.jsp").forward(request, response);
-		}else {
-			utilisateur = utilisateurDAOImpl.find(Integer.valueOf(request.getParameter("id")));
-			request.setAttribute("utilisateur", utilisateur);
-			this.getServletContext().getRequestDispatcher("/WEB-INF/modifierProfil.jsp").forward(request, response);
-		}
+		utilisateur = utilisateurDAOImpl.find(Integer.valueOf(request.getParameter("id")));
+		request.setAttribute("utilisateur", utilisateur);
+		this.getServletContext().getRequestDispatcher("/WEB-INF/modifierProfil.jsp").forward(request, response);
 	}
 
 	/**
 	 * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse response)
 	 */
 	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-		doGet(request, response);
+		utilisateur = utilisateurDAOImpl.find(Integer.valueOf(request.getParameter("utilisateur")));
+		utilisateur.setPseudo(request.getParameter("pseudo"));
+		utilisateur.setNom(request.getParameter("nom"));
+		utilisateur.setPrenom(request.getParameter("prenom"));
+		utilisateur.setEmail(request.getParameter("email"));
+		utilisateur.setTelephone(request.getParameter("telephone"));
+		utilisateur.setRue(request.getParameter("rue"));
+		utilisateur.setCodePostal(request.getParameter("codePostal"));
+		utilisateur.setVille(request.getParameter("ville"));
+		if(!request.getParameter("newmdp").equals("")) {
+			if(request.getParameter("mdp").equals(utilisateur.getMotDePasse())) {
+				if(request.getParameter("newmdp").equals(request.getParameter("confirmation"))) {
+					utilisateur.setMotDePasse(request.getParameter("newmdp"));
+				}else {
+					request.setAttribute("message", "Votre mot de passe de confirmation et votre nouveau mot de passe ne correspondent pas.");
+					request.setAttribute("utilisateur", utilisateur);
+					this.getServletContext().getRequestDispatcher("/WEB-INF/modifierProfil.jsp").forward(request, response);
+				}
+			}else {
+				request.setAttribute("message", "Votre mot de passe actuel est incorrect.");
+				request.setAttribute("utilisateur", utilisateur);
+				this.getServletContext().getRequestDispatcher("/WEB-INF/modifierProfil.jsp").forward(request, response);
+			}
+		}
+		UtilisateurDAOImpl utilisateurDAOImpl = new UtilisateurDAOImpl();
+		utilisateurDAOImpl.update(utilisateur);
+		request.setAttribute("message", "Votre profil a été modifié avec succès.");
+		request.setAttribute("utilisateur", utilisateur);
+		this.getServletContext().getRequestDispatcher("/WEB-INF/modifierProfil.jsp").forward(request, response);
 	}
 
 }
