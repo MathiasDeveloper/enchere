8c8b302063a2db6bb73149fc738ffe2e6eacd4ed
Florent <launay.florent22@gmail.com>
2020-04-09 10:38:38 +0200

    modifier profil

--- src/fr/eni/encheres/servlet/HomeServlet.java
+++ src/fr/eni/encheres/servlet/HomeServlet.java
@@ -1,6 +1,8 @@
 package fr.eni.encheres.servlet;
 
+import javax.servlet.RequestDispatcher;
 import javax.servlet.annotation.WebServlet;
+
 import java.io.IOException;
 
 /**
@@ -14,6 +16,13 @@ public class HomeServlet extends javax.servlet.http.HttpServlet {
     }
 
     protected void doGet(javax.servlet.http.HttpServletRequest request, javax.servlet.http.HttpServletResponse response) throws javax.servlet.ServletException, IOException {
-
+	request.setAttribute("login", "BAILLE");
+		
+		String url  = request.getContextPath();
+		System.out.println(url);
+		
+		RequestDispatcher rd = request.
+				getRequestDispatcher("/index.jsp");
+		rd.forward(request, response);
     }
 }
