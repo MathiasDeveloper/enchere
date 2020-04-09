ea57afc2065019490c259e9e93e9cbecdbd9ec6f
Dorig <dorig.bernard.db@gmail.com>
2020-04-09 20:17:30 +0200

    Page d'accueil - Création de la servlet redirigeant + jsp listeEnchereConnecte

--- src/fr/eni/encheres/servlet/HomeServlet.java
+++ src/fr/eni/encheres/servlet/HomeServlet.java
@@ -1,6 +1,8 @@
 package fr.eni.encheres.servlet;
 
 import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.Cookie;
+
 import java.io.IOException;
 
 /**
@@ -11,6 +13,17 @@ import java.io.IOException;
 public class HomeServlet extends javax.servlet.http.HttpServlet {
 
     protected void doGet(javax.servlet.http.HttpServletRequest request, javax.servlet.http.HttpServletResponse response) throws javax.servlet.ServletException, IOException {
-    	
+    	boolean connecte=false;
+    	Cookie[] cookies = request.getCookies();
+		for(Cookie unCookie:cookies)
+		{
+			if(unCookie.getName().equals("idUtilisateur")) {
+				connecte=true;
+				this.getServletContext().getRequestDispatcher("/WEB-INF/listeEnchereConnecte.jsp").forward(request, response);
+			}
+		}
+		if(connecte==false) {
+			this.getServletContext().getRequestDispatcher("/seConnecter").forward(request, response);
+		}
     }
 }
