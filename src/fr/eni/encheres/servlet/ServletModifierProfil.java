0f6748fbfdf3ece01fdef9b1a5b1b5bdbd29c137
Dorig <dorig.bernard.db@gmail.com>
2020-04-09 10:32:18 +0200

    Modifier Profil - Optimisation du code au niveau de la servlet

--- src/fr/eni/encheres/servlet/ServletModifierProfil.java
+++ src/fr/eni/encheres/servlet/ServletModifierProfil.java
@@ -31,14 +31,7 @@ public class ServletModifierProfil extends HttpServlet {
 	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
 	 */
 	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-		try {
-			utilisateur = utilisateurDAOImpl.find(Integer.valueOf(request.getParameter("id")));
-		} catch (NumberFormatException e) {
-			this.getServletContext().getRequestDispatcher("/WEB-INF/utilisateurInconnu.jsp").forward(request, response);
-		} catch (BuisnessException e) {
-			e.printStackTrace();
-		}
-		if(utilisateur.getPseudo().equals("")) {
+		if(verifierExistenceUtilisateur(request, "id")==false) {
 			this.getServletContext().getRequestDispatcher("/WEB-INF/utilisateurInconnu.jsp").forward(request, response);
 		}
 		request.setAttribute("utilisateur", utilisateur);
@@ -49,11 +42,45 @@ public class ServletModifierProfil extends HttpServlet {
 	 * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse response)
 	 */
 	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-		try {
-			utilisateur = utilisateurDAOImpl.find(Integer.valueOf(request.getParameter("utilisateur")));
-		}catch (BuisnessException e) {
-			e.printStackTrace();
+		if(verifierExistenceUtilisateur(request, "utilisateur")==false) {
+			this.getServletContext().getRequestDispatcher("/WEB-INF/utilisateurInconnu.jsp").forward(request, response);
 		}
+		if(verifierTelephone(request)==false) {
+			request.setAttribute("message", "Votre numéro de téléphone doit uniquement être constitué de 10 chiffres.");
+			request.setAttribute("utilisateur", utilisateur);
+			this.getServletContext().getRequestDispatcher("/WEB-INF/modifierProfil.jsp").forward(request, response);
+		}
+		switch(verifierMotDePasse(request)) {
+		case 1: //newmdp#confirmation
+			request.setAttribute("message", "Votre mot de passe de confirmation et votre nouveau mot de passe ne correspondent pas.");
+			request.setAttribute("utilisateur", utilisateur);
+			this.getServletContext().getRequestDispatcher("/WEB-INF/modifierProfil.jsp").forward(request, response);
+			break;
+		case 2: //mdp incorrect
+			request.setAttribute("message", "Votre mot de passe actuel est incorrect.");
+			request.setAttribute("utilisateur", utilisateur);
+			this.getServletContext().getRequestDispatcher("/WEB-INF/modifierProfil.jsp").forward(request, response);
+			break;
+		}
+		if(verifierExistenceUtilisateur(request, "utilisateur")==true && verifierTelephone(request)==true && (verifierMotDePasse(request)==0 || verifierMotDePasse(request)==3)) {
+			if(verifierMotDePasse(request)==3) {
+				modifierUtilisateur(request, false); //on ne change pas le mdp
+			}else {
+				modifierUtilisateur(request, true); //on change le mdp
+			}
+			try {
+				utilisateurDAOImpl.update(utilisateur);
+			} catch (BuisnessException e) {
+				e.printStackTrace();
+			}
+			request.setAttribute("message", "Votre profil a été modifié avec succès.");
+			request.setAttribute("utilisateur", utilisateur);
+			this.getServletContext().getRequestDispatcher("/WEB-INF/modifierProfil.jsp").forward(request, response);
+		}
+	}
+	
+	//Méthode modifier Utilisateur
+	private void modifierUtilisateur(HttpServletRequest request, boolean mdp){
 		utilisateur.setPseudo(request.getParameter("pseudo"));
 		utilisateur.setNom(request.getParameter("nom"));
 		utilisateur.setPrenom(request.getParameter("prenom"));
@@ -62,30 +89,54 @@ public class ServletModifierProfil extends HttpServlet {
 		utilisateur.setRue(request.getParameter("rue"));
 		utilisateur.setCodePostal(request.getParameter("codePostal"));
 		utilisateur.setVille(request.getParameter("ville"));
+		if(mdp) {
+			utilisateur.setMotDePasse(request.getParameter("newmdp"));
+		}
+	};
+	
+	//Méthode vérifier Existence Utilisateur
+	private boolean verifierExistenceUtilisateur(HttpServletRequest request, String parametre) {
+		try {
+			utilisateur = utilisateurDAOImpl.find(Integer.valueOf(request.getParameter(parametre)));
+			if(utilisateur.getPseudo()==null) {
+				return false;
+			}
+			return true;
+		} catch (NumberFormatException e) {
+			return false;
+		} catch (BuisnessException e) {
+			e.printStackTrace();
+			return false;
+		}
+	};
+	
+	//Méthode vérifier Téléphone
+	private boolean verifierTelephone(HttpServletRequest request) {
+		if(request.getParameter("telephone").length()==10) {
+			try {
+			    int telephone = Integer.parseInt(request.getParameter("telephone"));
+			    return true;
+			} catch (NumberFormatException e) {
+				return false;
+			}
+		}else {
+			return false;
+		}
+	};
+	
+	//Méthode vérifiant le mdp (0=ok, 1=newmdp#confirmation, 2=mdp incorrect, 3=ne pas changer le mdp)
+	private int verifierMotDePasse(HttpServletRequest request) {
 		if(!request.getParameter("newmdp").equals("")) {
 			if(request.getParameter("mdp").equals(utilisateur.getMotDePasse())) {
 				if(request.getParameter("newmdp").equals(request.getParameter("confirmation"))) {
-					utilisateur.setMotDePasse(request.getParameter("newmdp"));
+					return 0;
 				}else {
-					request.setAttribute("message", "Votre mot de passe de confirmation et votre nouveau mot de passe ne correspondent pas.");
-					request.setAttribute("utilisateur", utilisateur);
-					this.getServletContext().getRequestDispatcher("/WEB-INF/modifierProfil.jsp").forward(request, response);
+					return 1;
 				}
 			}else {
-				request.setAttribute("message", "Votre mot de passe actuel est incorrect.");
-				request.setAttribute("utilisateur", utilisateur);
-				this.getServletContext().getRequestDispatcher("/WEB-INF/modifierProfil.jsp").forward(request, response);
+				return 2;
 			}
 		}
-		UtilisateurDAOImpl utilisateurDAOImpl = new UtilisateurDAOImpl();
-		try {
-			utilisateurDAOImpl.update(utilisateur);
-		} catch (BuisnessException e) {
-			e.printStackTrace();
-		}
-		request.setAttribute("message", "Votre profil a été modifié avec succès.");
-		request.setAttribute("utilisateur", utilisateur);
-		this.getServletContext().getRequestDispatcher("/WEB-INF/modifierProfil.jsp").forward(request, response);
+		return 3;
 	}
-
 }
