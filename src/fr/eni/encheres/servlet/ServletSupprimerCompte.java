467361e2f24a83bcaac2d39015297d7cc45127b6
Dorig <dorig.bernard.db@gmail.com>
2020-04-09 15:26:40 +0200

    Supprimer Compte - Ajout de la deconnexion

--- src/fr/eni/encheres/servlet/ServletSupprimerCompte.java
+++ src/fr/eni/encheres/servlet/ServletSupprimerCompte.java
@@ -3,6 +3,7 @@ package fr.eni.encheres.servlet;
 import java.io.IOException;
 import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.Cookie;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
@@ -33,6 +34,13 @@ public class ServletSupprimerCompte extends HttpServlet {
 			UtilisateurManager utilisateurManager = new UtilisateurManager();
 			try {
 				utilisateurManager.delete(utilisateurManager.find(Integer.valueOf(request.getParameter("id"))));
+				Cookie[] cookies = request.getCookies();
+				for(Cookie unCookie:cookies)
+    			{
+    				if(unCookie.getName().equals("idUtilisateur")) {
+    					unCookie.setMaxAge(0);
+    				}
+    			}
 				this.getServletContext().getRequestDispatcher("/WEB-INF/accueil.jsp").forward(request, response);
 			} catch (NumberFormatException e) {
 				this.getServletContext().getRequestDispatcher("/WEB-INF/utilisateurInconnu.jsp").forward(request, response);
