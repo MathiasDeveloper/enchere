e07b573a47c92d4857fa2a46007fe728a7f19297
Dorig <dorig.bernard.db@gmail.com>
2020-04-10 09:25:36 +0200

    Page Accueil - Correction cookies + bootstrap

--- src/fr/eni/encheres/servlet/HomeServlet.java
+++ src/fr/eni/encheres/servlet/HomeServlet.java
@@ -9,21 +9,25 @@ import java.io.IOException;
  * Servlet Home
  */
 
- @WebServlet( name="HomeServlet", urlPatterns = {"/"} )
+ @WebServlet( name="HomeServlet", urlPatterns = {"/Home"} )
 public class HomeServlet extends javax.servlet.http.HttpServlet {
 
     protected void doGet(javax.servlet.http.HttpServletRequest request, javax.servlet.http.HttpServletResponse response) throws javax.servlet.ServletException, IOException {
     	boolean connecte=false;
-    	Cookie[] cookies = request.getCookies();
-		for(Cookie unCookie:cookies)
-		{
-			if(unCookie.getName().equals("idUtilisateur")) {
-				connecte=true;
-				this.getServletContext().getRequestDispatcher("/ListeEnchereConnecte").forward(request, response);
-			}
-		}
-		if(connecte==false) {
-			this.getServletContext().getRequestDispatcher("/seConnecter").forward(request, response);
-		}
+    	try{
+    		Cookie[] cookies = request.getCookies();
+    		for(Cookie unCookie:cookies){
+    			if(unCookie.getName().equals("idUtilisateur")) {
+    				connecte=true;
+    				this.getServletContext().getRequestDispatcher("/ListeEnchereConnecte").forward(request, response);
+    			}
+    		}
+    		if(connecte==false) {
+    			this.getServletContext().getRequestDispatcher("/seConnecter").forward(request, response);
+    		}
+    	}catch(Exception e) {
+    		this.getServletContext().getRequestDispatcher("/seConnecter").forward(request, response);
+    	}
+		
     }
 }
