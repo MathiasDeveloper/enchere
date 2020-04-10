788f79782c96abe2450de41302b6fea33a58d970
Mathias Robert <mathias@azumadesign.fr>
2020-04-10 10:06:36 +0200

    Page d'accueil - Correction et optimisation de code
     -> Correction sur un problème d'initialisation de cookie

--- src/fr/eni/encheres/servlet/HomeServlet.java
+++ src/fr/eni/encheres/servlet/HomeServlet.java
@@ -14,20 +14,18 @@ public class HomeServlet extends javax.servlet.http.HttpServlet {
 
     protected void doGet(javax.servlet.http.HttpServletRequest request, javax.servlet.http.HttpServletResponse response) throws javax.servlet.ServletException, IOException {
     	boolean connecte=false;
-    	try{
-    		Cookie[] cookies = request.getCookies();
-    		for(Cookie unCookie:cookies){
-    			if(unCookie.getName().equals("idUtilisateur")) {
-    				connecte=true;
-    				this.getServletContext().getRequestDispatcher("/ListeEnchereConnecte").forward(request, response);
-    			}
-    		}
-    		if(connecte==false) {
-    			this.getServletContext().getRequestDispatcher("/seConnecter").forward(request, response);
-    		}
-    	}catch(Exception e) {
-    		this.getServletContext().getRequestDispatcher("/seConnecter").forward(request, response);
-    	}
-		
+		Cookie[] cookies = request.getCookies();
+		if (cookies != null) {
+			for(Cookie unCookie:cookies)
+			{
+				if(unCookie.getName().equals("idUtilisateur")) {
+					connecte=true;
+					this.getServletContext().getRequestDispatcher("/ListeEnchereConnecte").forward(request, response);
+				}
+			}
+			if(connecte==false) {
+				this.getServletContext().getRequestDispatcher("/seConnecter").forward(request, response);
+			}
+		}
     }
 }
