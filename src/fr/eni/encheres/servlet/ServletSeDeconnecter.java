83cbc75a842bec89ee0316570f66afe1b667c5b5
chamaello <chamaello@192.168.0.19>
2020-04-14 15:51:54 +0200

    Mise en place de SeDéco

---
+++ src/fr/eni/encheres/servlet/ServletSeDeconnecter.java
@@ -0,0 +1,44 @@
+package fr.eni.encheres.servlet;
+
+import java.io.IOException;
+import java.io.PrintWriter;
+
+import javax.servlet.RequestDispatcher;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.Cookie;
+import javax.servlet.http.HttpSession;
+
+import fr.eni.encheres.bll.UtilisateurManager;
+import fr.eni.encheres.bo.Utilisateur;
+import fr.eni.encheres.dal.UtilisateurDAOImpl;
+import fr.eni.encheres.outils.BuisnessException;
+
+/**
+ * Servlet Home
+ */
+
+ @WebServlet( name="ServletVerificationPourLaConnection", urlPatterns = {"/seConnecter"} )
+public class ServletSeDeconnecter extends javax.servlet.http.HttpServlet {
+    /**
+	 * 
+	 */
+	private static final long serialVersionUID = 1L;
+	private static UtilisateurManager utilisateurManager = UtilisateurManager.getInstance();
+
+	protected void doPost(javax.servlet.http.HttpServletRequest request, javax.servlet.http.HttpServletResponse response) throws javax.servlet.ServletException, IOException {
+    	doGet(request, response);
+    }
+
+    protected void doGet(javax.servlet.http.HttpServletRequest request, javax.servlet.http.HttpServletResponse response) throws javax.servlet.ServletException, IOException {
+    	// Recupere la session
+    	HttpSession session = request.getSession(true);
+    	
+    	if(request.getParameter("idUtilisateur") != null ){  
+            session.invalidate();
+            RequestDispatcher rd = request.getRequestDispatcher("/WEB-INF/jsp/formulaireSeConnecter.jsp");
+        	rd.forward(request, response);
+        }
+    	
+    
+    }
+}
